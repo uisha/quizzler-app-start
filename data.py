26bf013a12ebdaab8409225f796f5c68fb2bf6a3
uisha <joshua_emmanuel_g_tipon@dlsu.edu.ph>
2023-08-27 20:22:27 +0800

    UI Done

--- data.py
+++ data.py
@@ -1,8 +1,7 @@
 import requests
-
 parameters = {
     "amount": 10,
     "type": "boolean"
 }
-response = requests.get(url="https://opentdb.com/api.php?", params=parameters)
+response = requests.get(url="https://opentdb.com/api.php", params=parameters)
 question_data = response.json()["results"]
--- ui.py
+++ ui.py
@@ -1,2 +1,33 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+
+class QuizInterface:
+
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(bg=THEME_COLOR, padx=20, pady=20)
+
+        self.score_label = Label(text="Score: 0", bg=THEME_COLOR, fg="white")
+        self.score_label.grid(column=1, row=0)
+
+        self.canvas = Canvas(height=250, width=300, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            text="Some Question Text",
+            font=("Arial", 20, "italic"),
+            fill=THEME_COLOR)
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+
+        true_img = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=self.true_img, highlightthickness=0)
+        self.true_button.grid(column=0, row=2)
+
+        false_img = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=self.false_img, highlightthickness=0)
+        self.false_button.grid(column=1, row=2)
+
+        self.window.mainloop()
